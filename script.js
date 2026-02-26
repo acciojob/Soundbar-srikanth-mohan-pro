c700df3b7c03e02da9dd721d61f1661c349f7e28
Srikanth Mohan <srikanth.mohan.98@gmail.com>
2026-02-26 17:18:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const buttons=document.querySelectorAll(".btn");
 const stop=document.querySelector(".stop");
 let currentAudio=null;
 
-buttons.forEach(button,()=>{
+buttons.forEach((button),()=>{
 	button.addEventListener("click",function(){
 		if(currentAudio){
 			currentAudio.pause();
