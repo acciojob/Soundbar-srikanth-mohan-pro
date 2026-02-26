bc50e64f958bc09c83a7d0362f110c50acf84483
Srikanth Mohan <srikanth.mohan.98@gmail.com>
2026-02-26 17:14:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const buttons=document.querySelectorAll(".btn");
+const stop=document.querySelector(".stop");
+let currentAudio=null;
+
+buttons.forEach(button,()=>{
+	button.addEventListener("click",function(){
+		if(currentAudio){
+			currentAudio.pause();
+			currentAudio.currentTime=0;
+		}
+
+		const SoundName=button.innerText;
+
+			currentAudio=new Audio("sounds/"+SoundName+".mp3");
+
+		currentAudio.play();
+	});
+});
+
+stop.addEventListener("click",function(){
+	if(currentAudio){
+		currentAudio.pause();
+		currentAudio.currentTime=0;
+	}
+});
+
